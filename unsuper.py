62068c66f178a9db1ff8b8401c472ceed74a5766
Haree <hareyadav898977@gmail.com>
2026-04-02 16:41:24 +0530

    unsuper update

--- unsuper.py
+++ unsuper.py
@@ -25,18 +25,18 @@ with st.form('my-form'):
         prediction = model.predict(input_data)
         st.write('cluster',prediction[0])
 
-    if prediction[0] == 0:
-        st.write('sorry! You could not placed')
-    elif prediction[0] == 1 or prediction[0] == 2 or prediction[0] == 3 or prediction[0] == 4:
-        st.write('placed')
-    elif prediction[0] == 5:
-        st.write('60 to 70 percent chance to place')
-    elif prediction[0] == 6:
-        st.write('may be')
-    elif prediction[0] == 7 or prediction[0] ==8 or prediction[0]== 9:
-        st.write('out of placement criteria')
-    else:
-        st.write('Try Next time')
+        if prediction[0] == 0:
+            st.write('sorry! You could not placed')
+        elif prediction[0] == 1 or prediction[0] == 2 or prediction[0] == 3 or prediction[0] == 4:
+            st.write('placed')
+        elif prediction[0] == 5:
+            st.write('60 to 70 percent chance to place')
+        elif prediction[0] == 6:
+            st.write('may be')
+        elif prediction[0] == 7 or prediction[0] == 8 or prediction[0] == 9:
+            st.write('out of placement criteria')
+        else:
+            st.write('Try Next time')
 
 centers = scaler.inverse_transform(model.cluster_centers_)
 

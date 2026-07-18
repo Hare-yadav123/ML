a6a0f42cf41fc916f6a61faafbd6c57f48a85713
Hare Yadav <hareyadav898977@gmail.com>
2026-07-18 15:01:32 +0530

    survive file updated

--- survive.py
+++ survive.py
@@ -2,12 +2,31 @@ import pickle
 import streamlit as st 
 import pandas as pd 
 
+df = pd.read_csv('titanic.csv')
+
 try:
     model = pickle.load(open('modelusersurvive.pk','rb'))
 except Exception as e:
     st.error(f'error is {e}')
 
 st.title('model for survive prediction')
+
+Pclass1 = sorted(df['Pclass1'].unique())
+Age1 = sorted(df['Age1'].unique())
+SibSp1 = sorted(df['SibSp1'].unique())
+Parch1 = sorted(df['Parch1'].unique())
+Fare1 = sorted(df['Fare1'].unique())
+Sex1 = sorted(df['Sex1'].unique())
+Embarked1 = sorted(df['Embarked1'].unique())
+
+Pclass = st.selectbox('Pclass',Pclass1)
+Age = st.selectbox('Age',Age1)
+SibSp = st.selectbox('sibSp',SibSp1)
+Parch = st.selectbox('Parch',Parch1)
+Fare = st.selectbox('Fare',Fare1)
+Sex = st.selectbox('Sex',Sex1)
+Embarked = st.selectbox('Embarked',Embarked1)
+
 
 Pclass = st.number_input('Pclass')
 Age = st.number_input('Age')
